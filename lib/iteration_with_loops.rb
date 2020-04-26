5ad81affd7272d3a67fccc51f0141ce1407945f8
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-26 23:31:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ def find_min_in_nested_arrays(src)
   while count_index < src.count do 
     count_element = 0 
     max_value = -1
-    min_value = 0 
+    min_value = nil
     
     while count_element < src[count_index].count do
       
